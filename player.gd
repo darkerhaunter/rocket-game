b6d81844f9f5f176f32f8622994650700eae6398
Ryan Mattox <509528@gmail.com>
2021-04-06 22:23:45 -0700

    camera rotation

---
+++ player.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+func _process(delta):
+	if Input.is_action_just_pressed("rotate_left"):
+		rotation -= PI / 2
+	elif Input.is_action_just_pressed("rotate_right"):
+		rotation += PI / 2
